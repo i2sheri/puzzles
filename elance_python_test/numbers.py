7f1eadb9aa7f51949609ccd6de0541ff187dacdc
Sharath <i2sheri@gmail.com>
2014-04-24 11:09:10 +0800

    Create numbers.py

---
+++ elance_python_test/numbers.py
@@ -0,0 +1,11 @@
+"""
+Write a program which will find all such numbers which are divisible by 7 but are not a multiple of 5,
+between 1000 and 1200 (both included).
+The numbers obtained should be printed in a comma separated sequence on a single line.
+"""
+
+l=[]
+for n in range(1000, 1201):
+    if n%7 == 0 and n%5 != 0:
+        l.append(str(n))
+print ','.join(l)
