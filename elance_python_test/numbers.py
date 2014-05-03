013e447f901804cc8e0cee65916e3c2aa86226d0
Sharath <i2sheri@gmail.com>
2014-05-03 12:02:58 +0800

    Update numbers.py

--- elance_python_test/numbers.py
+++ elance_python_test/numbers.py
@@ -4,8 +4,4 @@ between 1000 and 1200 (both included).
 The numbers obtained should be printed in a comma separated sequence on a single line.
 """
 
-l=[]
-for n in range(1000, 1201):
-    if n%7 == 0 and n%5 != 0:
-        l.append(str(n))
-print ','.join(l)
+print ','.join([str(n) for n in range(1000, 1201) if n%7 == 0 and n%5 != 0])
