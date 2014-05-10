05d5a27c494d66e0cdc5808345293cac4c0c9177
Sharath <i2sheri@gmail.com>
2014-05-10 22:48:27 +0800

    Update value_meal.py

--- value_meal/value_meal.py
+++ value_meal/value_meal.py
@@ -2,9 +2,9 @@
 
 """Chose the best value meal in town."""
 
-import argparse
 import csv
 import itertools
+import sys
 
 __author__ = 'Sharath Kumar Suroju'
 __email__ = 'i2sheri@gmail.com'
@@ -17,9 +17,13 @@ class Restaurant(object):
     menu = {}
 
     __add = lambda self, op1, op2: op1 + op2
+    __red = lambda self, c_menu, c_order: reduce(self.__add,
+                                             (c_menu.get(o) for o in c_order))
 
     def __init__(self, menu_file):
         self.__menu_file = menu_file
+        self.price = None
+        self.restaurant = None
 
     def populate_menu(self):
         """Parse CSV file and populate City Menu."""
@@ -27,7 +31,7 @@ class Restaurant(object):
             reader = csv.reader(menu_file)
             for row in reader:
                 rest = int(row[0])
-                item, is_combo = self.__get_item(row)
+                item, is_combo = get_item(row)
                 rest_menu = self.menu.get(rest)
                 if is_combo:
                     if rest_menu:
@@ -44,14 +48,6 @@ class Restaurant(object):
                     else:
                         rest_menu = {item: float(row[1])}
                 self.menu[rest] = rest_menu
-
-
-    def __get_item(self, row):
-        """Parse input line."""
-        if len(row) == 3:
-            return row[2], False
-        else:
-            return tuple(row[2:]), True
 
     def __menu_exists(self, order):
         """Return restaurants which can serve the order.
@@ -79,15 +75,16 @@ class Restaurant(object):
                 menus.append(rest)
         return (menus, combos)
 
+    def __set_result(self, new_price, rest_id):
+        """Update price and restaurant."""
+        if not self.price or new_price < self.price:
+            self.price, self.restaurant = new_price, rest_id
+
     def __print_price(self, menus, order):
         """Print most economic price and restaurant of the order."""
-        price = None
-        restaurant = None
         for rest_id in menus[0]:
-            new_price = reduce(self.__add,
-                               (self.menu.get(rest_id).get(o) for o in order))
-            if not price or new_price < price:
-                price, restaurant = new_price, rest_id
+            new_price = self.__red(self.menu.get(rest_id), order)
+            self.__set_result(new_price, rest_id)
 
         for rest_id in menus[1]:
             mul_combos = []
@@ -99,45 +96,36 @@ class Restaurant(object):
                         for comb in itertools.combinations(iterable, i):
                             if (order <= set(reduce(self.__add, comb)) |
                                 set(rest_menu)):
-                                new_price = reduce(self.__add,
-                                           (iterable.get(c) for c in comb))
-                                if not price or new_price < price:
-                                    price, restaurant = new_price, rest_id
+                                new_price = self.__red(iterable, order)
+                                self.__set_result(new_price, rest_id)
                 else:
                     new_price = rest_menu.get('combo').get(c_option)
-                    if price and new_price > price:
+                    if self.price and new_price > self.price:
                         continue
                     else:
                         mul_combos.append((c_option, new_price))
-                        # c = [j for j in order if j in c_option]
                         r_order = order - set(c_option)
                         if r_order:
-                            new_price += reduce(self.__add,
-                                        (rest_menu.get(o) for o in r_order))
-                        if not price or new_price < price:
-                            price, restaurant = new_price, rest_id
+                            new_price += self.__red(rest_menu, r_order)
+                        self.__set_result(new_price, rest_id)
 
             if len(mul_combos) > 1:
                 for k in xrange(len(mul_combos), 1, -1):
                     for comb in itertools.combinations(mul_combos, k):
                         new_price = reduce(self.__add, (c[1] for c in comb))
-                        if price > new_price:
+                        if self.price > new_price:
                             r_order = order - set(reduce(self.__add,
                                                          (c[0] for c in comb)))
                             if r_order:
-                                new_price += reduce(self.__add,
-                                            (rest_menu.get(o) for o in r_order))
-                            if not price or new_price < price:
-                                price, restaurant = new_price, rest_id
+                                new_price += self.__red(rest_menu, r_order)
+                                self.__set_result(new_price, rest_id)
 
-        print '%s, %s' % (restaurant, price)
+        print '%s, %s' % (self.restaurant, self.price)
 
     def resolve_order(self, order):
         """Get Best Value Meal from all menus."""
         # Parse given XML menu and populate menu
         self.populate_menu()
-
-        order = set(order.strip().split(' '))
 
         # Get Restaurants which can serve order and possible ways of serving
         menus = self.__menu_exists(order)
@@ -147,20 +135,12 @@ class Restaurant(object):
         else:
             print 'Menu does not exist'
 
-
-def parse_arguments():
-    """Parse command line arguments."""
-    parser = argparse.ArgumentParser()
-    parser.add_argument('-m', '--menu', help="Menu CSV File", action="store")
-    parser.add_argument('-o', '--order',
-                        help="Place your order",
-                        action="store")
-    return parser.parse_args()
-
+def get_item(row):
+    """Parse input line."""
+    if len(row) == 3:
+        return row[2].strip(), False
+    else:
+        return tuple([x.strip() for x in row[2:]]), True
 
 if __name__ == "__main__":
-    __PARAMS = parse_arguments()
-    if __PARAMS.menu and __PARAMS.order:
-        Restaurant(__PARAMS.menu).resolve_order(__PARAMS.order)
-    else:
-        print "Please provide Menu CSV and order, use -h for help."
+    Restaurant(sys.argv[1]).resolve_order(set(sys.argv[2:]))
