4cee935bf64e7abd127992fcf187e48506cdfe64
Sharath <i2sheri@gmail.com>
2014-05-10 23:32:27 +0800

    Create level_1.py

---
+++ pythonchallenge.com/level_1.py
@@ -0,0 +1,27 @@
+# My original solution
+"""
+# import re
+small = re.compile('[a-z]')
+for s in question:
+    if re.match(small, s):
+        if s == 'z':
+            print 'b',
+        elif s == 'y':
+            print 'a',
+        else:
+            print chr(ord(s)+2),
+    else:
+        print s,
+"""
+
+import string
+
+QUESTION = ("g fmnc wms bgblr rpylqjyrc gr zw fylb. rfyrq ufyr amknsrcpq ypc "
+            "dmp. bmgle gr gl zw fylb gq glcddgagclr ylb rfyr'q ufw rfgq rcvr "
+            "gq qm jmle. sqgle qrpgle.kyicrpylq() gq pcamkkclbcb. lmu ynnjw "
+            "ml rfc spj.")
+
+
+# Recommended
+CIPHER = string.lowercase[:]
+print QUESTION.translate(string.maketrans(CIPHER, CIPHER[2:] + 'ab'))
