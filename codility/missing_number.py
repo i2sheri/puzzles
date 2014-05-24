dbb0829d1b9bc6e8ab0d81f11e5a4efd876c4309
Sharath <i2sheri@gmail.com>
2014-05-24 11:23:32 +0800

    Update missing_number.py

--- codility/missing_number.py
+++ codility/missing_number.py
@@ -1,20 +1,4 @@
-"""
-Array of distinct integers of size N is given.
-It contains integers in the range [1, (N + 1)].
-which means exactly one element is missing, find it.
-
-1 < N < 1,00,000
-
-Ex: [1, 2, 4, 6, 7, 8, 3]
-Output: 5
-
-Ex: [1, 3]
-Output: 2
-
-Complexity:
-expected worst-case time complexity is O(N)
-expected worst-case space complexity is O(1)
-"""
+#https://codility.com/programmers/lessons/PermMissingElem
 
 def missing_number(ip):
   n = len(ip)
